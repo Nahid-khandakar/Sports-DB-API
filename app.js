df9470c8425cdf00c47a11921c505923b11b68fd
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-02-28 17:46:06 +0600

    search and id passed

--- app.js
+++ app.js
@@ -6,6 +6,44 @@ const allPlayers = () => {
 
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => showPlayerDetails(data.player))
 
+}
+
+const showPlayerDetails = (players) => {
+
+    console.log(players)
+
+    for (const player of players) {
+        const parent = document.getElementById('player-container')
+
+
+        const div = document.createElement('div')
+        div.innerHTML = ` 
+        <div class="card border p-5 mb-3">
+    
+            <div class="pro-pic">
+                <img class="w-50" src="${player.strThumb}" alt="">
+            </div>
+    
+            <h2>Name:${player.strPlayer}</h2>
+            <h3>Country:${player.strTeam}</h3>
+            <p></p>
+    
+            <div class="allbutton">
+                <button class="btn btn-danger">Delete</button>
+                <button onclick="details(${player.idPlayer})" class="btn btn-success">Details</button>
+            </div>
+        </div>
+        `;
+        parent.appendChild(div)
+
+    }
+}
+
+const details = (id) => {
+    const url = `https://www.thesportsdb.com/api/v1/json/2/lookupplayer.php?id=${id}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data))
 }
